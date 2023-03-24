8219ecd6817170c0beab74c6872eaa8deef21e9e
AshuBtosh <banskota.ashutosh@gmail.com>
2023-03-24 07:37:28 +0545

    Done

---
+++ examkolagi.py
@@ -0,0 +1,15 @@
+import math
+
+class Circle:
+    def __init__(self, r):
+        r = 1
+
+    def config(self,r):
+        area =  22/7 *r**2
+        print('Area of circle is ', area)
+
+Circle.config(1,2)
+
+
+        
+
